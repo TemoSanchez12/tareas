103c9ac67423c152b73dc1c7936337bc53ae2f03
Cuauhtémoc Alejandro Sánchez Carrillo <batemo4912@gmail.com>
2022-03-13 21:49:17 -0600

    first commit

---
+++ Tarea2/Tarea2/Program.cs
@@ -0,0 +1,89 @@
+﻿public class Pelicula {
+    private String nombre;
+    private String categoria;
+    private int numeroEstante;
+
+    public Pelicula(String nombre, String categoria, int numeroEstante) {
+        this.categoria = categoria;
+        this.nombre = nombre;  
+        this.numeroEstante = numeroEstante;
+    }
+
+    public String toString() {
+        return "La pelicula " + this.nombre + " se encuentra en la estanteria " + this.numeroEstante;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public String getCategoria() {
+        return this.categoria;
+    }
+
+    public int getEstante() {
+        return this.numeroEstante;
+    }
+
+}
+
+public class Libreria {
+    private List<Pelicula> peliculas;
+
+    public Libreria() {
+        this.peliculas = new List<Pelicula>();
+    }
+
+    public bool agregarPelicula() {
+        String nombre = Console.ReadLine();
+        String categoria = Console.ReadLine();
+        String numeroEstante = Console.ReadLine();
+
+        if (nombre == null || categoria == null || numeroEstante == null) {
+            return false;
+        }
+
+        int numeroEstanteEntero = Int32.Parse(numeroEstante);
+
+        Pelicula pelicula = new Pelicula(nombre, categoria, numeroEstanteEntero);
+        this.peliculas.Add(pelicula);
+
+        return true;
+    }
+
+    public void obtenerPeliculaPorEstanteria() {
+        Console.WriteLine("Ingrese el numero de la estanteria que te interesa: ");
+        int numeroEstanteria = Int32.Parse(Console.ReadLine());
+
+        for (int i = 0; i < this.peliculas.Count; i++) {
+            Pelicula pelicula = this.peliculas[i];
+            if (pelicula.getEstante() == numeroEstanteria) {
+                Console.WriteLine(pelicula);
+            }
+        }
+    }
+
+    public void obtenerPeliculasPorCategoria() {
+        Console.WriteLine("Ingrese nombre de la categoria que te interesa: ");
+        String categoria = Console.ReadLine();
+
+        for (int i = 0; i < this.peliculas.Count; i++) {
+            Pelicula pelicula = this.peliculas[i];
+            if (pelicula.getCategoria() == categoria) {
+                Console.WriteLine(pelicula);
+            }
+        }
+    }
+
+    public void obtenerPeliculaPorNombre() {
+        Console.WriteLine("Ingrese el nombre de la pelicula que te interesa: ");
+        String nombre= Console.ReadLine();
+
+        for (int i = 0; i < this.peliculas.Count; i++) {
+            Pelicula pelicula = this.peliculas[i];
+            if (pelicula.getNombre() == nombre) {
+                Console.WriteLine(pelicula);
+            }
+        }
+    }
+}
---
+++ Tarea2Ejercicio2/Tarea2Ejercicio2/Program.cs
@@ -0,0 +1,65 @@
+﻿public class Producto {
+    private int id;
+    private String nombre;
+    private int ventas;
+
+    public Producto(int id, String nombre, int ventas) {
+        this.id = id;
+        this.nombre = nombre;
+        this.ventas = ventas;
+    }
+
+    public void agregarVenta() {
+        this.ventas = ventas + 1;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String toString() {
+        return "Nombre producto: " + this.nombre + ", ventas: " + this.ventas;
+    }
+}
+
+public class Inventario {
+    private List<Producto> productos;
+
+    public Inventario() {
+        productos = new List<Producto> ();
+    }
+
+    public bool agregarProducto() {
+        int id = Int32.Parse(Console.ReadLine());
+        String nombre = Console.ReadLine();
+        String categoria = Console.ReadLine();
+
+        if (nombre == null || id == null || categoria == null) {
+            return false;
+        }
+
+        this.productos.Add (new Producto(id, nombre, 0));
+
+        return true;
+    }
+
+    public bool agregarVenta() {
+        Console.WriteLine("Ingresa el id del producto que se vendio");
+        int id = Int32.Parse(Console.ReadLine());
+
+        for (int i = 0; i < this.productos.Count(); i++) {
+            Producto producto = this.productos[i];
+            if (producto.getId() == id) {
+                producto.agregarVenta();
+            }
+        }
+
+        return true;
+    }
+
+    public void mostrarProductos() {
+        for (int i = 0; i < this.productos.Count(); i++) {
+            Console.Write(this.productos[i]);
+        }
+    }
+}
